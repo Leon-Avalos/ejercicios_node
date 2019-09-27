aa140ab62ddffedefeca08d11d5bbbc7bc6808c0
Leon <heiko.avalos@gmail.com>
2019-09-27 15:40:55 -0500

    Base routes working!

--- router/router.js
+++ router/router.js
@@ -2,6 +2,22 @@ const express = require('express')
 
 var router = express.Router()
 
-router.get()
+router.get('/suma', (req, res ) => {
+    res.send("Sumar")
+})
+
+router.get('/resta', (req, res ) => {
+    res.send("Restar")
+})
+
+router.get('/multiplicacion', (req, res ) => {
+    res.send("Multiplicar")
+})
+
+router.get('/division', (req, res ) => {
+    res.send("Dividir")
+})
+
+
 
 module.exports = router
