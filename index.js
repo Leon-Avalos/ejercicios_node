848d607bd83046cb1ade1db279bb3f2c3a8c55af
Leon <heiko.avalos@gmail.com>
2019-09-27 15:40:16 -0500

    Router linked in main

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
 const express = require('express')
 var app = express();
-const router = require('./router/router')
-
-
-
-
-
-app.use('/', function (req, res, next) {
-    res.send('API working');
-    next()
-});
+const router = require('./router/router.js')
 
 app.use(router)
 
-app.get('/sumar/:n1/:n2', function (req, res) {
-    var n1 = parseInt(req.params.n1)
-    var n2 = parseInt(req.params.n2)
-    var suma = n1 + n2
-    var out = JSON.stringify({
-        "data" : {
-            "numero 1": n1,
-            "numero 2": n2, 
-            "respuesta": suma
-        }       
-    })
-    res.send(out).status(200)
-
+app.use('/', (req, res, next) => {
+    console.log("Paso por aca")
+    next()
 });
+
+
+
+app.use("/", (req, res) =>{
+    console.log("No se encontro")
+})
+
 
 app.listen(3000, function () {
     console.log('App listening on port 3000!');
